e0a2b6773893730cf64fdd3e79a76e521e6f622f
illusion_ <thortwo@outlook.com>
2018-08-04 18:30:01 +0800

    spider add

--- JianShu/settings.py
+++ JianShu/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'JianShu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'station',
+#         'USER': 'root',
+#         'PASSWORD': '123456',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'station',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
+        'USER': 'station',
+        'PASSWORD': 'password',
+        'HOST': '23.110.211.170',
         'PORT': '3306',
     }
 }
