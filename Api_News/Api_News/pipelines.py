bff2f76150f3165b0fce770118a99d13a9a1b3bf
illusion_ <thortwo@outlook.com>
2018-08-04 17:59:26 +0800

    基本功能实现

---
+++ Api_News/Api_News/pipelines.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from .items import JSModel_newsarticle
+from Api_News.Api_News.items import ApiNewsItem, NewsImageItem
+from scrapy.pipelines.images import ImagesPipeline
+from scrapy.exceptions import DropItem
+import scrapy
+from peewee import *
+from JSModel.models import NewsArticle
+
+
+class ApiNewsPipeline(object):
+
+    def process_item(self, item, spider):
+        if isinstance(item, ApiNewsItem):
+            page = NewsArticle()
+            page.re_id = item['re_id']
+            page.author = item['author']
+            page.title = item['title']
+            page.keyword = item['keyword']
+            page.description = item['description']
+            page.create_time = item['crate_time']
+            page.quantity = item['quantity']
+            page.read = item['read']
+            page.comment = item['comment']
+            page.like =item['like']
+            page.img = item['img']
+            page.videoUrls = item['videoUrls']
+            page.context = item['context']
+            page.url = item['url']
+            page.tips = item['tips']
+            page.source = item['source']
+            page.save()
+            return item
+
+
+class MyImagesPipeline(ImagesPipeline):
+    """自定义图片下载器,以图片url为路径保存图片"""
+    def get_media_requests(self, item, info):
+        """
+        发生图片下载请求,其中item['front_image_url']字段是scrapy中我们自定义的url字段,
+        以数组的方式存储,遍历数组请求图片
+        """
+        if isinstance(item, NewsImageItem):
+            for image_url in item['image_urls']:
+                yield scrapy.Request(image_url)
+
+    # def item_completed(self, results, item, info):
+    #     """
+    #     results是一个list 第一个为图片下载状态,对应OK  第二个是一个tupled其中可以为path的字段对应存储路径,
+    #     而item['front_image_path']是我们自定义字段,保存在item中
+    #     """
+    #     if isinstance(item, NewsImageItem):
+    #         front_image_path = [x['path'] for ok, x in results if ok]
+    #         if not front_image_path:
+    #             raise DropItem("Item contains no images")
+    #         item['front_image_path'] = front_image_path
+    #         return item
+
+    def file_path(self, request, response=None, info=None):
+        """自定义图片保存路径,以图片的url保存,重写前是图片的url经过MD5编码后存储"""
+        image_guid = request.url
+        name = str(image_guid).split('/')[-1]
+        return 'full/%s' % name
+
---
+++ Api_News/Api_News/settings.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for Api_News project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://doc.scrapy.org/en/latest/topics/settings.html
+#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+
+BOT_NAME = 'Api_News'
+
+SPIDER_MODULES = ['Api_News.spiders']
+NEWSPIDER_MODULE = 'Api_News.spiders'
+
+# MYSQL_HOST = '103.56.136.105'
+# MYSQL_DBNAME = 'news'
+# MYSQL_USER = 'root'
+# MYSQL_PASSWD = 'password'
+# MYSQL_PORT = 3306
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'Api_News (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = True
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+CONCURRENT_REQUESTS = 32
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+DOWNLOAD_DELAY = 0.25
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+DEFAULT_REQUEST_HEADERS = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'accept-language': 'zh-CN,zh;q=0.8',
+    # 'referer': 'https://47.90.63.143/',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36”)',
+    # 'Host': '47.90.63.143'
+}
+
+# Enable or disable spider middlewares
+# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'Api_News.middlewares.ApiNewsSpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    'Api_News.middlewares.ApiNewsDownloaderMiddleware': 543,
+#}
+
+# Enable or disable extensions
+# See https://doc.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+   'Api_News.pipelines.ApiNewsPipeline': 300,
+   'Api_News.pipelines.MyImagesPipeline': 100,
+}
+# IMAGES_STORE = '/www/wwwroot/Special/JianShu/JSModel/templates/JSModel/static/img/'
+IMAGES_STORE = '/Users/hexiaotian/PycharmProjects/JianShu/JSModel/templates/JSModel/static/img/'
+# 该字段的值为XxxItem中定义的存储图片链接的image_urls字段
+IMAGES_URLS_FIELD = 'image_urls'
+# 该字段的值为XxxItem中定义的存储图片信息的images字段
+IMAGES_RESULT_FIELD = 'images'
+# 生成缩略图(可选)
+# IMAGES_THUMBS = {
+#     'small': (50, 50),
+#     'big': (270, 270),
+# }
+# 过期时间,单位:天(可选)
+IMAGES_EXPIRES = 120
+# 过滤小图片(可选)
+# IMAGES_MIN_HEIGHT = 110
+# IMAGES_MIN_WIDTH = 110
+# 是否允许重定向(可选)
+# MEDIA_ALLOW_REDIRECTS = True
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+# AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
