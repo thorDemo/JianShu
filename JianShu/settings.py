18ea73241e8f6a2c918151be160c2dceac30c958
illusion_ <thortwo@outlook.com>
2018-08-09 21:23:21 +0800

    添加日志分析后台

--- JianShu/settings.py
+++ JianShu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^bn+f6d-_7b5qbc4&i6nb#-)u3t9cz-rtz4-#njmc53-8x_zk2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'www.ahs33.cn', '23.110.211.170', 'm.ahs33.cn', 'ubuntu:8000']
+ALLOWED_HOSTS = ['127.0.0.1', 'www.aht33.cn', '142.234.255.29', 'm.aht33.cn', 'ubuntu:8000']
 
 
 # Application definition
@@ -75,26 +75,26 @@ WSGI_APPLICATION = 'JianShu.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'station',
-#         'USER': 'station',
-#         'PASSWORD': 'password',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'station',
-        'USER': 'root',
-        'PASSWORD': '123456',
+        'USER': 'station',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'station',
+#         'USER': 'root',
+#         'PASSWORD': '123456',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
