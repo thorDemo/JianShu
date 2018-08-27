a6edd8fbb6e464a05a261bb0ecff0067529062c7
illusion_ <thortwo@outlook.com>
2018-08-27 17:07:33 +0800

    img

--- JianShu/urls.py
+++ JianShu/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('JSModel.urls')),
     path('admin/', admin.site.urls),
+    path('', include('JSModel.urls')),
 ]
